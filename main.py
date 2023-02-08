24d80b3f8f116176317fd5aa9188f32a8db9f46d
muyeon-jo <muyeonjo97@gmail.com>
2023-02-09 00:46:01 +0900

    임베딩 작업 준비

--- main.py
+++ main.py
@@ -8,8 +8,20 @@ from torch.nn import functional as F
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
 import datetime
 import os
+import pickle
+
+def pickle_load(name):
+    with open(name, 'rb') as f:
+        data = pickle.load(f)
+    return data
+
+def pickle_save(data, name):
+    with open(name, 'wb') as f:
+        pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
 
 # show grid image
 def imshow_grid(img):
@@ -33,7 +45,6 @@ class VAE(nn.Module):
 
         self.fc3 = nn.Linear(latent_size, hidden_size_1)
         self.fc4 = nn.Linear(hidden_size_1, image_size)
-
     def encode(self, x):
         h1 = F.tanh(self.fc1(x))
         
@@ -49,12 +60,12 @@ class VAE(nn.Module):
         return torch.sigmoid(self.fc4(h3))
 
     def forward(self, x):
-        mu, logvar = self.encode(x.view(-1, 784))
+        mu, logvar = self.encode(x.view(-1, 14000))
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
 
 def loss_function(recon_x, x, mu, logvar):
-    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction = 'sum')
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 14000), reduction = 'sum')
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
     return BCE, KLD
 def train(epoch, model, train_loader, optimizer):
@@ -109,7 +120,7 @@ def test(epoch, model, test_loader):
 
             if batch_idx == 0:
                 n = min(data.size(0), 8)
-                comparison = torch.cat([data[:n], recon_batch.view(BATCH_SIZE, 1, 28, 28)[:n]]) # (16, 1, 28, 28)
+                comparison = torch.cat([data[:n], recon_batch.view(BATCH_SIZE, 14000)[:n]]) # (16, 1, 28, 28)
                 grid = torchvision.utils.make_grid(comparison.cpu()) # (3, 62, 242)
                 writer.add_image("Test image - Above: Real data, below: reconstruction data", grid, epoch)
 def test_other_input(model,test_data):
@@ -133,7 +144,50 @@ def latent_to_image(epoch, model):
         sample = torch.randn(64, 2).to(DEVICE)
         recon_image = model.decode(sample).cpu()
         grid = torchvision.utils.make_grid(recon_image.view(64, 1, 28, 28))
-        writer.add_image("Latent To Image", grid, epoch)                        
+        writer.add_image("Latent To Image", grid, epoch)        
+def getLatentVector(model, input):
+    with torch.no_grad():
+        mu, logvar = model.encode(input)
+        z = model.reparameterize(mu, logvar)
+        return z
+class CustomDataset(Dataset):
+    def __init__(self, data, label):
+        self.x_data = data
+        self.y_data = label
+    
+    #데이터 총 개수 리턴 
+    def __len__(self):
+        return len(self.x_data)
+    
+    #인덱스에 해당되는 데이터를 tensor로 리턴 
+    def __getitem__(self, idx):
+        x = torch.FloatTensor(self.x_data[idx])
+        y = torch.FloatTensor(self.y_data[idx])
+        return x,y              
+
+def makeData():
+    f = pickle_load("./content/POI(philadelphia)/userVisitDataPerArea.pkl")
+    label = []
+    data = []
+    for i,row in f.items():
+        for j, v in row.items():
+            if len(v) == 0:
+                continue
+            li = []
+            sortedList = sorted(v.items(),key=lambda item: item[1])
+            s = 0
+            for q in sortedList:
+                li.append(q[1])
+                s+=q[1]
+            for q in range(len(li)):
+                li[q]= li[q]/s
+            for q in range(14000-len(sortedList)):
+                li.append(0)
+            data.append(li)
+            label.append([i,j])
+
+    pickle_save(data, "./content/POI(philadelphia)/normalizedUserVisitData.pkl")
+    pickle_save(label, "./content/POI(philadelphia)/normalizedUserVisitData_label.pkl")
 if __name__ == "__main__":
     USE_CUDA = torch.cuda.is_available()
     DEVICE = torch.device("cuda" if USE_CUDA else "cpu")
@@ -147,37 +201,54 @@ if __name__ == "__main__":
 
     print("저장 위치: ", saved_loc)
 
+   
+    
     writer = SummaryWriter(saved_loc)
-    EPOCHS = 50
-    BATCH_SIZE = 200
+    EPOCHS = 25
+    BATCH_SIZE = 100
     # Transformer code
     transformer = transforms.Compose([transforms.ToTensor()])
 
+    data = pickle_load("./content/POI(philadelphia)/normalizedUserVisitData.pkl")
+    label = pickle_load("./content/POI(philadelphia)/normalizedUserVisitData_label.pkl")
+    np.random.shuffle(data)
+    tr = data[:int(len(data)/10*8)]
+    #tr_label = label[:int(len(label)/10*8)]
+    te = data[int(len(data)/10*8):]
+    #te_label = label[:int(len(label)/10*8)]
+    trainset = CustomDataset(tr,tr)
+    trainloader = DataLoader(trainset, batch_size = BATCH_SIZE, shuffle = True, num_workers = 2)
+
+    testset = CustomDataset(te,te)
+    testloader = DataLoader(testset, batch_size = BATCH_SIZE, shuffle = True, num_workers = 2)
     # Loading trainset, testset and trainloader, testloader
-    trainset = torchvision.datasets.MNIST(root = './content/MNIST', train = True,
-                                            download = True, transform = transformer)
+    # trainset = torchvision.datasets.MNIST(root = './content/MNIST', train = True,
+    #                                         download = True, transform = transformer)
 
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size = BATCH_SIZE, shuffle = True, num_workers = 2)
+    # trainloader = DataLoader(trainset, batch_size = BATCH_SIZE, shuffle = True, num_workers = 2)
 
 
-    testset = torchvision.datasets.MNIST(root = './content/MNIST', train = False,
-                                            download = True, transform = transformer)
+    # testset = torchvision.datasets.MNIST(root = './content/MNIST', train = False,
+    #                                         download = True, transform = transformer)
 
-    testloader = torch.utils.data.DataLoader(testset, batch_size = BATCH_SIZE, shuffle = True, num_workers = 2)
+    # testloader = DataLoader(testset, batch_size = BATCH_SIZE, shuffle = True, num_workers = 2)
+    print(len(testset))
+    print(len(trainset))
+    print(len(testloader))
+    print(len(trainloader))
     # sample check
-    sample, label = next(iter(trainloader))
-
+    #sample, label = next(iter(trainloader))
     #imshow_grid(sample[0:8])
 
-    VAE_model = VAE(28*28, 512, 2).to(DEVICE)
+    VAE_model = VAE(14000, 2048, 512).to(DEVICE)
     optimizer = optim.Adam(VAE_model.parameters(), lr = 1e-3)
     #test_other_input(VAE_model, np.ones(28*28))
     for epoch in tqdm(range(0, EPOCHS)):
         train(epoch, VAE_model, trainloader, optimizer)
         test(epoch, VAE_model, testloader)
         print("\n")
-        latent_to_image(epoch, VAE_model)
-    test_other_input(VAE_model, np.ones(28*28))
+        #latent_to_image(epoch, VAE_model)
+    #test_other_input(VAE_model, np.ones(28*28))
     
     writer.close()
-    
+    pickle_save(VAE_model ,saved_loc+"/VAE_model.pkl")
